1eae7aef2e6474c34ccd272cbdddfcda61a32f8c
Shams Kitz <dustractor@gmail.com>
2023-09-25 19:06:35 -0500

    on help menu

--- deck.py
+++
@@ -1,9 +0,0 @@
-from random import randrange
-def strat():
-        with open("deck.txt") as f:
-            data = f.readlines()
-            return data[randrange(len(data))].strip()
-if __name__ == "__main__":
-    print()
-    print(strat())
-    print()
